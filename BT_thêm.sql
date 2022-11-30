771cd1f42f2d7c7992fb71ee86f77cd040fc8dcb
Tunguyen09CNTT03 <118881853+Tunguyen09CNTT03@users.noreply.github.com>
2022-11-30 21:06:45 +0700

    Add files via upload

---
+++ BT_thêm.sql
@@ -0,0 +1,12 @@
+﻿create trigger trg_KhongXoa
+	on NhanVien
+	for delete
+as
+	if exists(select *from deleted where DCHI like '%HCM%')
+	begin
+		print N'Không thể xóa nhân viên ở HCM'
+		rollback transaction; 
+	end;
+select *from NHANVIEN;
+
+delete from NHANVIEN where MANV = '123'
